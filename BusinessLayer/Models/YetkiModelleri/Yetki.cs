7f3b1ee756bb9217afa72abe4af90afa425a3354
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-11-22 16:30:28 +0300

    Case-62 Tamamlandı.

---
+++ BusinessLayer/Models/YetkiModelleri/Yetki.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models.YetkiModelleri
+{
+    public class Yetki
+    {
+
+        public int? YetkiId { get; set; }
+
+        public int? RotaId { get; set; }
+        
+        public bool? GirebilirMi { get; set; }
+
+        public KullaniciModelleri.KullaniciModel Kullanici { get; set; }
+
+        public String RotaAdi { get; set; }
+    }
+}
