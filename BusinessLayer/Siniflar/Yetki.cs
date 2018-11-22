96cb942bf2cae43907c868e9b2dea899767f27d4
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-11-22 16:26:43 +0300

    Case-61 Tamamlandı.

---
+++ BusinessLayer/Siniflar/Yetki.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Models.KullaniciModelleri;
+using BusinessLayer.Models.OrtakModeller;
+using DataLayer;
+namespace BusinessLayer.Siniflar
+{
+    public class Yetki
+    {
+        private DataLayer.Siniflar.Yetki yetkiDAL = new DataLayer.Siniflar.Yetki();
+
+    }
+}
---
+++ SosyalYardimProje/Controllers/YetkiController.cs
@@ -0,0 +1,19 @@
+﻿using SosyalYardimProje.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SosyalYardimProje.Controllers
+{
+    public class YetkiController : Controller
+    {
+        [KullaniciLoginFilter]
+        [HttpGet]
+        public ActionResult Liste()
+        {
+            return View();
+        }
+    }
+}
