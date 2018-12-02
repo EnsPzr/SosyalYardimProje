78f6f22fb50585161354cae2e245dd9ce24f38e2
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-03 02:12:11 +0300

    Case-79 Tamamlandı.

--- BusinessLayer/Siniflar/IhtiyacSahibi.cs
+++ BusinessLayer/Siniflar/IhtiyacSahibi.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using BusinessLayer.Models.IhtiyacSahibiModelleri;
+using DataLayer.Siniflar;
 namespace BusinessLayer.Siniflar
 {
     public class IhtiyacSahibi
     {
+        private IhtiyacSahibi ihtiyacSahibiDAL = new IhtiyacSahibi();
+
+        public List<IhtiyacSahibiModel> TumIhtiyacSahipleriniGetir(int? KullaniciId)
+        {
+            var ihtiyacSahipleri = ihtiyacSahibiDAL.TumIhtiyacSahipleriniGetir(KullaniciId);
+            var dondurulecekIhtiyacSahipleri=new List<IhtiyacSahibiModel>();
+            for (int i = 0; i < ihtiyacSahipleri.Count; i++)
+            {
+                IhtiyacSahibiModel eklenecekIhtiyacSahibi= new IhtiyacSahibiModel()
+                {
+                    IhtiyacSahibiAciklama=ihtiyacSahipleri[i].IhtiyacSahibiAciklama,
+                    IhtiyacSahibiAdi=ihtiyacSahipleri[i].IhtiyacSahibiAdi,
+                    IhtiyacSahibiAdres=ihtiyacSahipleri[i].IhtiyacSahibiAdres,
+                    IhtiyacSahibiId=ihtiyacSahipleri[i].IhtiyacSahibiId,
+                    IhtiyacSahibiSoyadi=ihtiyacSahipleri[i].IhtiyacSahibiSoyadi,
+                    IhtiyacSahibiTelNo=ihtiyacSahipleri[i].IhtiyacSahibiTelNo,
+                    Sehir= new Models.OrtakModeller.SehirModel()
+                    {
+                        SehirAdi=ihtiyacSahipleri[i].Sehir.SehirAdi,
+                        SehirId=ihtiyacSahipleri[i].Sehir.SehirId
+                    }
+                };
+                dondurulecekIhtiyacSahipleri.Add(eklenecekIhtiyacSahibi);
+            }
+
+            return dondurulecekIhtiyacSahipleri;
+        }
     }
 }
--- DataLayer/Siniflar/IhtiyacSahibi.cs
+++ DataLayer/Siniflar/IhtiyacSahibi.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.Entity;
 namespace DataLayer.Siniflar
 {
     public class IhtiyacSahibi
     {
+        private KullaniciYonetimi kullaniciDAL= new KullaniciYonetimi();
+        private SosyalYardimDB db = new SosyalYardimDB();
+        public List<IhtiyacSahibiTablo> TumIhtiyacSahipleriniGetir(int? KullaniciId)
+        {
+            if (kullaniciDAL.KullaniciBul(KullaniciId).KullaniciMerkezdeMi == true)
+            {
+                return db.IhtiyacSahibiTablo.Include(p=>p.SehirTablo).ToList();
+            }
+            else
+            {
+                return db.IhtiyacSahibiTablo.Include(p => p.SehirTablo).Where(p =>
+                    p.SehirTablo_SehirId == kullaniciDAL.KullaniciBul(KullaniciId).SehirTablo_SehirId).ToList();
+            }
+        }
     }
 }
