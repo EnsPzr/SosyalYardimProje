72945db03535d7b13950161a388a2585c8b885b5
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-03 01:44:42 +0300

    Case-76 Tamamlandı.

---
+++ SosyalYardimProje/Controllers/IhtiyacSahibiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SosyalYardimProje.Controllers
+{
+    public class IhtiyacSahibiController : Controller
+    {
+        public ActionResult Liste()
+        {
+            return View();
+        }
+    }
+}
