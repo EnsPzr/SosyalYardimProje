31dbfec25014fb905fb7e75cf6666ab240cffdfe
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-19 15:24:57 +0300

    Case-176 Tamamlandı.

---
+++ SosyalYardimProje/Controllers/EsyaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SosyalYardimProje.Controllers
+{
+    public class EsyaController : Controller
+    {
+        // GET: Esya
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
