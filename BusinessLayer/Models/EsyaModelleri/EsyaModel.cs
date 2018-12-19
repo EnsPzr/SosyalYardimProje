7f3aac3ebd199d0b7d25d5f4ce9eaf17709e77c0
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-19 15:29:37 +0300

    Case-177 Tamamlandı.

---
+++ BusinessLayer/Models/EsyaModelleri/EsyaModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models.EsyaModelleri
+{
+    public class EsyaModel
+    {
+        [Key]
+        public int? EsyaId { get; set; }
+
+        [Required(ErrorMessage = "Eşya adı alanı zorunludur.")]
+        [MinLength(3,ErrorMessage = "Eşya adı en az {1} karakter uzunluğunda olabilir."), MaxLength(30, ErrorMessage = "Eşya adı en fazla {1} karakter uzunluğunda olabilir.")]
+        [Display(Name = "Eşya Adı")]
+        public String EsyaAdi { get; set; }
+    }
+}
