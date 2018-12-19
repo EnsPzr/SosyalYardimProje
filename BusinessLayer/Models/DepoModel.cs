4aa7202f0953fe77a32e61c9232a9799f1f4c2ab
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-19 19:31:12 +0300

    Case-125 Tamamlandı.

---
+++ BusinessLayer/Models/DepoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Models.EsyaModelleri;
+using BusinessLayer.Models.OrtakModeller;
+
+namespace BusinessLayer.Models
+{
+    public class DepoModel
+    {
+        public int? DepoEsyaId { get; set; }
+
+        public EsyaModel Esya { get; set; }
+
+        public SehirModel Sehir { get; set; }
+
+        public int? Adet { get; set; }
+    }
+}
