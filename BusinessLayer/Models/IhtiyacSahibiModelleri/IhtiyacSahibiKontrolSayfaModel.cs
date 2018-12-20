20a4b4fb5e8192dcfa87bfcf59d910957730cc68
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-20 17:09:45 +0300

    Case-107 Tamamlandı.

---
+++ BusinessLayer/Models/IhtiyacSahibiModelleri/IhtiyacSahibiKontrolSayfaModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models.IhtiyacSahibiModelleri
+{
+    public class IhtiyacSahibiKontrolSayfaModel
+    {
+        public int? IhtiyacSahibiKontrolId { get; set; }
+        [Display(Name ="Ad Soyad")]
+        public String IhtiyacSahibiAdiSoyadi { get; set; }
+
+        [Display(Name = "Tel")]
+        public String IhtiyacSahibiTel { get; set; }
+
+        [Display(Name = "Adres")]
+        public String IhtiyacSahibiAdres { get; set; }
+
+        [Display(Name = "İl")]
+        public String IhtiyacSahibiIl { get; set; }
+
+        [Display(Name="Muhtaç Mı?")]
+        public bool? MuhtacMi { get; set; }
+
+        public List<IhtiyacSahibiVerileceklerModel> verileceklerList { get; set; }
+
+        [Display(Name = "Tahmini Teslim")]
+        public DateTime? TahminiTeslim { get; set; }
+
+        [Display(Name ="Nakdi Bağış")]
+        public Decimal? NakdiBagisMiktari { get; set; }
+    }
+}
---
+++ BusinessLayer/Models/IhtiyacSahibiModelleri/IhtiyacSahibiVerileceklerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models.IhtiyacSahibiModelleri
+{
+    public class IhtiyacSahibiVerileceklerModel
+    {
+        public int? EsyaId { get; set; }
+
+        public int? Adet { get; set; }
+    }
+}
