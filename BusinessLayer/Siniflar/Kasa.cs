da0312ec1cddb9261a6f27583296aa97234ec55e
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-22 00:43:31 +0300

    Case-199 Tamamlandı.

--- BusinessLayer/Siniflar/Kasa.cs
+++ BusinessLayer/Siniflar/Kasa.cs
@@ -3,10 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLayer.Models.KasaModelleri;
+using BusinessLayer.Models.OrtakModeller;
 
 namespace BusinessLayer.Siniflar
 {
     public class Kasa
     {
+        private DataLayer.Siniflar.Kasa kasaDAL = new DataLayer.Siniflar.Kasa();
+        public List<KasaModel> TumKasaGetir(int? kullaniciId)
+        {
+            var kasa = kasaDAL.TumKasaGetir(kullaniciId);
+            if (kasa.Count == 0)
+            {
+                return new List<KasaModel>();
+            }
+            else
+            {
+                var kasaList = kasa.Select(p => new KasaModel()
+                {
+                    Aciklama = p.Aciklama,
+                    KasaId = p.KasaId,
+                    KullaniciAdiSoyadi = p.KullaniciBilgileriTablo.KullaniciAdi + " " +
+                                           p.KullaniciBilgileriTablo.KullaniciSoyadi,
+                    KullaniciId = p.KullaniciBilgleriTablo_KullaniciId,
+                    Miktar = Convert.ToDouble(p.Miktar),
+                    Sehir = new SehirModel()
+                    {
+                        SehirAdi = p.SehirTablo.SehirAdi,
+                        SehirId = p.SehirTablo_SehirId
+                    },
+                    Tarih = p.Tarih
+                }).ToList();
+                return kasaList;
+            }
+        }
+
+        public List<KasaModel> FiltreliKasaGetir(int? kullaniciId, string aranan, string tarih, int? sehirId)
+        {
+            var kasa = kasaDAL.FiltreliKasaGetir(kullaniciId, aranan, tarih, sehirId);
+            if (kasa.Count == 0)
+            {
+                return new List<KasaModel>();
+            }
+            else
+            {
+                var kasaList = kasa.Select(p => new KasaModel()
+                {
+                    Aciklama = p.Aciklama,
+                    KasaId = p.KasaId,
+                    KullaniciAdiSoyadi = p.KullaniciBilgileriTablo.KullaniciAdi + " " +
+                                         p.KullaniciBilgileriTablo.KullaniciSoyadi,
+                    KullaniciId = p.KullaniciBilgleriTablo_KullaniciId,
+                    Miktar = Convert.ToDouble(p.Miktar),
+                    Sehir = new SehirModel()
+                    {
+                        SehirAdi = p.SehirTablo.SehirAdi,
+                        SehirId = p.SehirTablo_SehirId
+                    },
+                    Tarih = p.Tarih
+                }).ToList();
+                return kasaList;
+            }
+        }
     }
 }
--- DataLayer/Siniflar/Kasa.cs
+++ DataLayer/Siniflar/Kasa.cs
@@ -3,10 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace DataLayer.Siniflar
 {
     public class Kasa
     {
+        private KullaniciYonetimi KullaniciDAL = new KullaniciYonetimi();
+        private SosyalYardimDB db = new SosyalYardimDB();
+        public List<KasaTablo> TumKasaGetir(int? kullaniciId)
+        {
+            if (KullaniciDAL.KullaniciMerkezdeMi(kullaniciId))
+            {
+                return db.KasaTablo.Include(p => p.KullaniciBilgileriTablo).Include(p => p.SehirTablo).ToList();
+            }
+            else
+            {
+                int? kullaniciSehirId = KullaniciDAL.KullaniciSehir(kullaniciId);
+                return db.KasaTablo.Include(p => p.KullaniciBilgileriTablo).Include(p => p.SehirTablo).
+                    Where(p => p.SehirTablo_SehirId == kullaniciSehirId).ToList();
+            }
+        }
+
+        public List<KasaTablo> FiltreliKasaGetir(int? kullaniciId, string aranan, string tarih, int? sehirId)
+        {
+            if (KullaniciDAL.KullaniciMerkezdeMi(kullaniciId))
+            {
+                var sorgu = db.KasaTablo.Include(p => p.KullaniciBilgileriTablo).Include(p => p.SehirTablo)
+                    .AsQueryable();
+                if (aranan != null)
+                {
+                    sorgu = sorgu.Where(p => p.KullaniciBilgileriTablo.KullaniciAdi.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciSoyadi.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciEPosta.Contains(aranan)
+                                             || p.Miktar.ToString().Contains(aranan)
+                                             || p.SehirTablo.SehirAdi.Contains(aranan)
+                                             || p.Aciklama.Contains(aranan)
+                                             || p.Tarih.ToString().Contains(aranan));
+                }
+
+                if (tarih != null)
+                {
+                    sorgu = sorgu.Where(p => p.Tarih == Convert.ToDateTime(tarih));
+                }
+
+                if (sehirId != null)
+                {
+                    sorgu = sorgu.Where(p => p.SehirTablo_SehirId == sehirId);
+                }
+
+                return sorgu.ToList();
+            }
+            else
+            {
+                int? kullaniciSehirId = KullaniciDAL.KullaniciSehir(kullaniciId);
+                var sorgu = db.KasaTablo.Include(p => p.KullaniciBilgileriTablo).Include(p => p.SehirTablo)
+                    .Where(p=>p.SehirTablo_SehirId==kullaniciSehirId).AsQueryable();
+                if (aranan != null)
+                {
+                    sorgu = sorgu.Where(p => p.KullaniciBilgileriTablo.KullaniciAdi.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciSoyadi.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciEPosta.Contains(aranan)
+                                             || p.Miktar.ToString().Contains(aranan)
+                                             || p.SehirTablo.SehirAdi.Contains(aranan)
+                                             || p.Aciklama.Contains(aranan)
+                                             || p.Tarih.ToString().Contains(aranan));
+                }
+
+                if (tarih != null)
+                {
+                    sorgu = sorgu.Where(p => p.Tarih == Convert.ToDateTime(tarih));
+                }
+
+                return sorgu.ToList();
+            }
+        }
     }
 }
