e3b75a3a3a827458c3b773277c885ef629b2ad85
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-22 00:52:19 +0300

    Case-198 Tamamlandı.

--- SosyalYardimProje/Controllers/KasaController.cs
+++ SosyalYardimProje/Controllers/KasaController.cs
@@ -1,16 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer.Models.KasaModelleri;
+using BusinessLayer.Siniflar;
 
 namespace SosyalYardimProje.Controllers
 {
     public class KasaController : Controller
     {
+        private Kasa kasaBAL = new Kasa();
         public ActionResult Liste()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult TumKasaGetir()
+        {
+            KasaJsModel model = new KasaJsModel()
+            {
+                BasariliMi = true,
+                KasaList = kasaBAL.TumKasaGetir(KullaniciBilgileriDondur.KullaniciId())
+            };
+            model.KasaSayisi = model.KasaList.Count;
+            Thread.Sleep(2000);
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult FiltreliKasaGetir(string aranan, string tarih, int? sehirId)
+        {
+            if (aranan.Equals(""))
+            {
+                aranan = null;
+            }
+
+            if (tarih.Equals(""))
+            {
+                tarih = null;
+            }
+            else
+            {
+                try
+                {
+                    Convert.ToDateTime(tarih);
+                }
+                catch (Exception)
+                {
+                    tarih = null;
+                }
+            }
+            KasaJsModel model = new KasaJsModel()
+            {
+                BasariliMi = true,
+                KasaList = kasaBAL.FiltreliKasaGetir(KullaniciBilgileriDondur.KullaniciId(), aranan, tarih, sehirId)
+            };
+            model.KasaSayisi = model.KasaList.Count;
+            Thread.Sleep(2000);
+            return Json(model, JsonRequestBehavior.AllowGet);
         }
     }
 }
