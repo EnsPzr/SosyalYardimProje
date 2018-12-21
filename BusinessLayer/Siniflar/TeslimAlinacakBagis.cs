aa38f9d40187f342d08f33533e1759141997a96b
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-21 19:28:22 +0300

    Case-165 Tamamlandı.

--- BusinessLayer/Siniflar/TeslimAlinacakBagis.cs
+++ BusinessLayer/Siniflar/TeslimAlinacakBagis.cs
@@ -9,7 +9,7 @@ namespace BusinessLayer.Siniflar
 {
     public class TeslimAlinacakBagis
     {
-        private DataLayer.Siniflar.TeslimAlinacakBagis tesDAL= new DataLayer.Siniflar.TeslimAlinacakBagis();
+        private DataLayer.Siniflar.TeslimAlinacakBagis tesDAL = new DataLayer.Siniflar.TeslimAlinacakBagis();
         public List<TeslimAlinacakBagisModel> TumBagislariGetir(int? kullaniciId)
         {
             var bagislar = tesDAL.TumBagislariGetir(kullaniciId);
@@ -60,6 +60,54 @@ namespace BusinessLayer.Siniflar
             return don;
         }
 
-       
+        public List<TeslimAlinacakBagisModel> FiltreliBagislariGetir(int? kullaniciId, int sehirId, String aranan, String tarih)
+        {
+            var bagislar = tesDAL.FiltreliBagislariGetir(kullaniciId, sehirId, aranan, tarih);
+            List<TeslimAlinacakBagisModel> don = new List<TeslimAlinacakBagisModel>();
+            for (int i = 0; i < bagislar.Count; i++)
+            {
+                var eklenecek = new TeslimAlinacakBagisModel();
+                eklenecek.BagisId = bagislar[i].BagisId;
+                eklenecek.BagisciAdiSoyadi = bagislar[i].KullaniciBilgileriTablo.KullaniciAdi + " " +
+                                             bagislar[i].KullaniciBilgileriTablo.KullaniciSoyadi;
+                eklenecek.BagisciTelNo = bagislar[i].KullaniciBilgileriTablo.KullaniciTelefonNumarasi;
+                eklenecek.BagisciAdres = bagislar[i].KullaniciBilgileriTablo.KullaniciAdres;
+                eklenecek.EklenmeTarihi = bagislar[i].EklenmeTarihi;
+                if (bagislar[i].OnaylandiMi != null)
+                {
+                    if (bagislar[i].OnaylandiMi == true)
+                    {
+                        eklenecek.OnaylandiMiStr = "Evet";
+                    }
+                    else
+                    {
+                        eklenecek.OnaylandiMiStr = "Hayır";
+                    }
+                }
+                else
+                {
+                    eklenecek.OnaylandiMiStr = "Hayır";
+                }
+
+                if (bagislar[i].TeslimAlindiMi != null)
+                {
+                    if (bagislar[i].TeslimAlindiMi == true)
+                    {
+                        eklenecek.TeslimAlindiMi = "Evet";
+                    }
+                    else
+                    {
+                        eklenecek.TeslimAlindiMi = "Hayır";
+                    }
+                }
+                else
+                {
+                    eklenecek.TeslimAlindiMi = "Hayır";
+                }
+                don.Add(eklenecek);
+            }
+
+            return don;
+        }
     }
 }
--- DataLayer/Siniflar/TeslimAlinacakBagis.cs
+++ DataLayer/Siniflar/TeslimAlinacakBagis.cs
@@ -24,5 +24,58 @@ namespace DataLayer.Siniflar
                     .Include(p => p.BagisDetayTablo.Select(q => q.BagisDetayResimTablo)).Where(p => p.KullaniciBilgileriTablo.SehirTablo_SehirId == sehirId).ToList();
             }
         }
+
+        public List<BagisTablo> FiltreliBagislariGetir(int? kullaniciId, int? sehirId, String aranan, String tarih)
+        {
+            if (kullaniciDAL.KullaniciMerkezdeMi(kullaniciId))
+            {
+                var sorgu = db.BagisTablo.Include(p => p.BagisDetayTablo).Include(p => p.KullaniciBilgileriTablo)
+                    .Include(p => p.BagisDetayTablo.Select(q => q.BagisDetayResimTablo)).AsQueryable();
+                if (sehirId != null)
+                {
+                    sorgu = sorgu.Where(p => p.KullaniciBilgileriTablo.SehirTablo_SehirId == sehirId);
+                }
+
+                if (aranan != null)
+                {
+                    sorgu = sorgu.Where(p => p.KullaniciBilgileriTablo.KullaniciAdi.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciSoyadi.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciAdres.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciEPosta.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciTelefonNumarasi.Contains(aranan));
+                }
+
+                if (tarih != null)
+                {
+                    DateTime? tarihDate = Convert.ToDateTime(tarih);
+                    sorgu = sorgu.Where(p => p.EklenmeTarihi == tarihDate
+                                             || p.TahminiTeslimAlmaTarihi == tarihDate);
+                }
+
+                return sorgu.ToList();
+            }
+            else
+            {
+                int? SehirId = kullaniciDAL.KullaniciSehir(kullaniciId);
+                var sorgu = db.BagisTablo.Include(p => p.BagisDetayTablo).Include(p => p.KullaniciBilgileriTablo)
+                    .Include(p => p.BagisDetayTablo.Select(q => q.BagisDetayResimTablo)).Where(p => p.KullaniciBilgileriTablo.SehirTablo_SehirId == sehirId).AsQueryable();
+                if (aranan != null)
+                {
+                    sorgu = sorgu.Where(p => p.KullaniciBilgileriTablo.KullaniciAdi.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciSoyadi.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciAdres.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciEPosta.Contains(aranan)
+                                             || p.KullaniciBilgileriTablo.KullaniciTelefonNumarasi.Contains(aranan));
+                }
+
+                if (tarih != null)
+                {
+                    DateTime? tarihDate = Convert.ToDateTime(tarih);
+                    sorgu = sorgu.Where(p => p.EklenmeTarihi == tarihDate
+                                             || p.TahminiTeslimAlmaTarihi == tarihDate);
+                }
+                return sorgu.ToList();
+            }
+        }
     }
 }
