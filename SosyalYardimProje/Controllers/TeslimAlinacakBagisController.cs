46d4dbd87dffd8a6c8155dca13fa737c748a51e0
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-21 18:28:00 +0300

    Case-160 Tamamlandı.

---
+++ SosyalYardimProje/Controllers/TeslimAlinacakBagisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SosyalYardimProje.Controllers
+{
+    public class TeslimAlinacakBagisController : Controller
+    {
+        // GET: TeslimAlinacakBagis
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
