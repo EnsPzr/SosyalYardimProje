84cd35155136ee4f8ae58914a9dd9423cc6994af
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-22 14:58:15 +0300

    Case-224 Tamamlandı.

--- SosyalYardimProje/Controllers/GeriBildirimController.cs
+++ SosyalYardimProje/Controllers/GeriBildirimController.cs
@@ -1,16 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer;
+using BusinessLayer.Models.GeriBildirimModelleri;
+using BusinessLayer.Siniflar;
 
 namespace SosyalYardimProje.Controllers
 {
     public class GeriBildirimController : Controller
     {
+        private Kullanici kullaniciBAL = new Kullanici();
+        private GeriBildirim geriBildirimBAL = new GeriBildirim();
         public ActionResult Liste()
         {
+            Tanimla();
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult TumGeriBildirimleriGetir()
+        {
+            GeriBildirimJsModel model = new GeriBildirimJsModel()
+            {
+                BasariliMi=true,
+                GeriBildirimList=geriBildirimBAL.TumGeriBildirimleriGetir(KullaniciBilgileriDondur.KullaniciId())
+            };
+            model.GeriBildirimSayisi = model.GeriBildirimList.Count;
+            Thread.Sleep(2000);
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        public void Tanimla()
+        {
+            var sehirlerSelect = kullaniciBAL.SehirleriGetir(KullaniciBilgileriDondur.KullaniciId()).Select(p =>
+                new SelectListItem()
+                {
+                    Text = p.SehirAdi,
+                    Value = p.SehirId.ToString()
+                }).ToList();
+            ViewBag.sehirlerSelect = sehirlerSelect;
         }
     }
 }
