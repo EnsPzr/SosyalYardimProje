76af14d7ec82c69cf0f67e97ec1de6937041f7a1
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-22 14:35:40 +0300

    Case-223 Tamamlandı.

---
+++ BusinessLayer/Models/GeriBildirimModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models
+{
+    public class GeriBildirimModel
+    {
+        [Key]
+        public int? GeriBildirimId { get; set; }
+        
+        [Display(Name = "Ad Soyad")]
+        public String KullaniciAdiSoyadi { get; set; }
+
+        [Display(Name = "Tel No")]
+        public String KullaniciTel { get; set; }
+
+        [Display(Name = "Konu")]
+        public String Konu { get; set; }
+
+        [Display(Name = "Mesaj")]
+        [DataType(DataType.MultilineText)]
+        public String Mesaj { get; set; }
+
+        [Display(Name = "Geri Bil. Durumu")]
+        public String DurumStr { get; set; }
+
+        [Display(Name = "Geri Bil. Durumu")]
+        [Required(ErrorMessage = "Geri Bildirim Durumu seçilmek zorundadır.")]
+        public int? DurumInt { get; set; }
+    }
+}
