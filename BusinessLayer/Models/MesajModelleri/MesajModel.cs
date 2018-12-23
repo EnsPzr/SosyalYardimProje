50fd51be49d4f8ce53491d57b71abe1d1b4e106a
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-23 20:40:57 +0300

    Case-235 Tamamlandı.

---
+++ BusinessLayer/Models/MesajModelleri/MesajModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models.MesajModelleri
+{
+    public class MesajModel
+    {
+        [Key]
+        public int? MesajId { get; set; }
+
+        public int? KullaniciId { get; set; }
+
+        [Display(Name = "Gönderen")]
+        public String KullaniciAdiSoyadi { get; set; }
+
+        [Required(ErrorMessage = "Alıcı seçilmek zorundadır")]
+        public int? AliciInt { get; set; }
+
+        [Display(Name = "Alıcı")]
+        public String AliciStr { get; set; }
+
+
+    }
+}
