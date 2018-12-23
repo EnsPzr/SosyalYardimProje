62532fef4b72d316dc338143d88bb9aa79f16d6f
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-23 20:38:09 +0300

    Case-234 Tamamlandı.

---
+++ SosyalYardimProje/Controllers/MesajController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SosyalYardimProje.Controllers
+{
+    public class MesajController : Controller
+    {
+        private Mesaj mesajBAL = new Mesaj();
+        private Kullanici kullaniciBAL = new Kullanici();
+        public ActionResult Liste()
+        {
+            return View();
+        }
+    }
+}
