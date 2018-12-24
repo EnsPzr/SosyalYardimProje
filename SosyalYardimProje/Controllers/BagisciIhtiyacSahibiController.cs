ea5042372e727e487fc840addaa6ac111439eb84
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-24 14:46:02 +0300

    Case-290-291 Tamamlandı.

---
+++ SosyalYardimProje/Controllers/BagisciIhtiyacSahibiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer.Siniflar;
+
+namespace SosyalYardimProje.Controllers
+{
+    public class BagisciIhtiyacSahibiController : Controller
+    {
+        private IhtiyacSahibi ihtiyacSahibiBAL = new IhtiyacSahibi();
+        public ActionResult Liste()
+        {
+            var ihtiyacSahipleri = ihtiyacSahibiBAL.TumIhtiyacSahipleriniGetir(BagisciBilgileriDondur.KullaniciId());
+            return View(ihtiyacSahipleri);
+        }
+    }
+}
