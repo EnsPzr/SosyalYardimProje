8ca0132ee91f87ff3a39d8d8fb6f35c682e72ace
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-24 20:53:20 +0300

    Case-266-237 Tamamlandı.

--- SosyalYardimProje/Controllers/BagisciBagisController.cs
+++ SosyalYardimProje/Controllers/BagisciBagisController.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using BusinessLayer.BagisciSiniflar;
+using BusinessLayer.Models.TeslimAlinacakBagis;
 
 namespace SosyalYardimProje.Controllers
 {
+    //full bağışçı
     public class BagisciBagisController : Controller
     {
         private BagisciBagis bagisBAL = new BagisciBagis();
         public ActionResult Liste()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult TumBagislariGetir()
+        {
+            TeslimAlinacakBagisJsModel model = new TeslimAlinacakBagisJsModel()
+            {
+                BasariliMi=true,
+                BagisList= bagisBAL.TumBagislariGetir(BagisciBilgileriDondur.KullaniciId())
+            };
+            model.BagisSayisi = model.BagisList.Count;
+            Thread.Sleep(2000);
+            return Json(model, JsonRequestBehavior.AllowGet);
         }
     }
 }
