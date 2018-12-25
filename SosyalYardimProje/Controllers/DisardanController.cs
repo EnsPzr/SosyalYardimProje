60fe8f911d5ae746512f5d4e8db13d0ee5dbf76c
Mehmet Enes Pazar <enespazar4342@gmail.com>
2018-12-25 13:01:59 +0300

    Case-327 Tamamlandı.

--- SosyalYardimProje/Controllers/DisardanController.cs
+++ SosyalYardimProje/Controllers/DisardanController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer;
+using BusinessLayer.Models.DisardanIhtiyacSahibiModelleri;
+using BusinessLayer.Siniflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,52 @@ namespace SosyalYardimProje.Controllers
 {
     public class DisardanController : Controller
     {
+        private Kullanici kullaniciBAL = new Kullanici();
+        private IhtiyacSahibi ihtiyacSahibiBAL = new IhtiyacSahibi();
         public ActionResult AnaSayfa()
         {
             return View();
+        }
+
+        public ActionResult IhtiyacSahibiEkle()
+        {
+            Tanimla();
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult IhtiyacSahibiEkle(DisardanIhtiyacSahibiModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var sonuc = ihtiyacSahibiBAL.DisardanIhtiyacSahibiKaydet(model);
+                if (sonuc.TamamlandiMi == true)
+                {
+                    TempData["uyari"] = "Teşekkür ederiz. Ekiplerimiz tarafında ihtiyaç sahibi ziyaret edilecektir. Girmiş olduğunuz bilgiler doğrultusunda sizin için hesap açılmıştır. Hesabınıza girip eşya bağışı yapabilir ve eklediğiniz ihtiyaç sahiplerini görebilirsiniz.";
+                    return RedirectToAction("AnaSayfa");
+                }
+                else
+                {
+                    String hatalar = KullaniciBilgileriDondur.HataMesajlariniOku(sonuc.HataMesajlari);
+                    Tanimla();
+                    return View(model);
+                }
+            }
+            else
+            {
+                Tanimla();
+                return View(model);
+            }
+        }
+
+        public void Tanimla()
+        {
+            var sehirlerSelectList = kullaniciBAL.TumSehirleriGetir().Select(p => new SelectListItem()
+            {
+                Text=p.SehirAdi,
+                Value=p.SehirId.ToString()
+            }).ToList();
+            ViewBag.sehirlerSelectList = sehirlerSelectList;
         }
     }
 }
